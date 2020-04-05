a0d4b99169797cd7c74aef15410029fe932bad60
Matix97 <mateusz_barsnski@outlook.com>
2020-04-05 17:28:27 +0200

    before changing for google formatter

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+
+        System.out.println("asdasd");
+    }
+
+    // ten zapis @@ -33,7 +33,6 @@ to mów
+}

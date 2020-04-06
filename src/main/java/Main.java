14c608bbc64dd736de841a6c3cb9cf0d4ba00aa2
Matix97 <mateusz_barsnski@outlook.com>
2020-04-06 10:15:04 +0200

    adding some code to make documentation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,47 @@
+/**
+ * Main class which runs the whole application
+ *
+ * @author Mateusz
+ * @version 1.0
+ * @since 2020-03-31
+ */
 public class Main {
+  /** Sample first data */
+  private int x;
+
+  /** Sample second data */
+  private double sasda;
+
+  /** Sample third data */
+  private String sa = "asdasdasd";
+
+  /**
+   * This is the main method which makes use of addNum method.
+   *
+   * @param args Unused.
+   * @return Nothing.
+   */
   public static void main(String[] args) {
     System.out.println("Hello world");
 
     System.out.println("asdasd");
   }
 
-  // ten zapis @@ -33,7 +33,6 @@ to mów
+  /**
+   * Gets the first and last name of this Student.
+   *
+   * @return this Student's name.
+   */
+  public String getName() {
+    return "name";
+  }
+
+  /**
+   * Changes the name of this Student. This may involve a lengthy legal process.
+   *
+   * @param newName This Student's new name. Should include both first and last name.
+   */
+  public void setName(String newName) {
+    System.out.println(newName);
+  }
 }
